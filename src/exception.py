a9934828697d62ce76e6bb9789b63d37db2ec0c3
sufyan.arahman <sufyan.abdulrahman55@gmail.com>
2025-07-28 17:45:49 +0500

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -17,7 +17,7 @@ class CustomException(Exception):
         self.error_message = error_message_details(
             error_message, error_detail=error_detail
         )
-
+        
     def __str__(self):
         return self.error_message
 
